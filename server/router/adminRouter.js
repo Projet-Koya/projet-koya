1a5b6e78a483cd6faaa04f8c3bef5de945c0acad
Rahmad3067 <aboozar919@gmail.com>
2021-10-06 17:04:48 +0200

    article get added

--- server/router/adminRouter.js
+++ server/router/adminRouter.js
@@ -1,13 +1,13 @@
 const express = require("express");
 const adminContoller = require("../controller/adminController");
 const router = express.Router();
-const protect  = require("../middleware/protect")
+// const protect  = require("../middleware/protect")
 const { addUser, deleteUser, addlogin, changeUserInfo, userLogout } = require("../controller/adminController");
 
-router.post("/user", protect,  addUser)
-router.delete("/user/:id", protect, deleteUser)
+router.post("/user", addUser)
+router.delete("/user/:id", deleteUser)
 router.post("/user/login", addlogin)
-router.put("/user/change/:email", protect, changeUserInfo)
+router.put("/user/change/:email", changeUserInfo)
 router.get("/user/logout", userLogout)
 
 
