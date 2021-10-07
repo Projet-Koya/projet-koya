ec78a674b1aa01f4b2ccff1399efb040a7d09d1b
baydir <kidoni@live.fr>
2021-10-07 13:46:51 +0200

    user edit

--- src/Components/Admin.js
+++ src/Components/Admin.js
@@ -1,17 +1,18 @@
 import React from 'react';
 import { LoginContext } from '../App';
 import { useContext } from 'react';
+import UserInfo from './UserInfo'
 
 const Admin = () => {
-    // const LoginStatus = useContext(LoginContext);
-    // if (LoginStatus.LogStatus !== true) {
-    //     console.log("Pas connecté");
-    //     return null;
-    // }
-    // console.log("test")
+    const LoginStatus = useContext(LoginContext);
+    if (LoginStatus.LogStatus !== true) {
+        console.log("Pas connecté");
+        return null;
+    }
     return (
         <div>
             <h1>Page d'admin</h1>
+            <UserInfo/>
         </div>
     );
 };
