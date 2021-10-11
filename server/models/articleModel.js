2b61a09e6582c1d06a97ba22d6e13cc3982e5dae
baydir <kidoni@live.fr>
2021-10-11 14:15:36 +0200

    ajout fonctionnalité post article

--- server/models/articleModel.js
+++ server/models/articleModel.js
@@ -5,12 +5,11 @@ const ArticleSchema = new mongoose.Schema({
     categoryID: { type: mongoose.Types.ObjectId, ref: "Category" },
     userID: { type: mongoose.Types.ObjectId, ref: "User" },
     title: String,
-    text: String,
-    date: String,
+    text: String
 }
-,{
-	timestamps: true,
-});
+    , {
+        timestamps: true,
+    });
 
 
 const Article = mongoose.model("Article", ArticleSchema);
