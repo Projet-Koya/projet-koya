69a4ec643d23969edcb98d2c1e31faab35c0f4ef
juj624 <julienpenot@outlook.com>
2021-10-12 10:59:46 +0200

    comment

--- server/models/commentModel.js
+++ server/models/commentModel.js
@@ -5,6 +5,7 @@ const CommentSchema = new mongoose.Schema({
     articleID: { type: mongoose.Types.ObjectId, ref: "article" },
     author: String,
     text: String,
+    isValid: { type: Boolean, default: false },
 }
     , {
         timestamps: true,
