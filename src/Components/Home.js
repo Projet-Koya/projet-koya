49f4f794f91cd724f81cb0fd09c747a1b7b8b377
Rahmad3067 <aboozar919@gmail.com>
2021-10-13 10:21:38 +0200

    Added Carousel

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,12 +1,25 @@
 import React from 'react'
-import Popular from './lastArticle/LastArticle'
+import CategorySlider from '../Components/Carousels/CategoryCarousel'
+import ArticleCarousel from '../Components/Carousels/ArticleCaousel';
 
 
 const Home = () => {
     return(
-        <div className="bg-gray-50">
-            <Popular />
-            
+        <div className="container  my-20">
+            <div className="grid grid-cols-2 md:grid-cols-2">
+                <div className="">
+                    <img src="https://via.placeholder.com/400/" alt=""/>
+                </div>
+                
+                <div className="">
+                    <ArticleCarousel />
+                </div>
+            </div>
+
+            <div className="m:container mx-auto my-24">
+                <CategorySlider />
+            </div>
+             
         </div>
     )
 }
