5964d28aee8ce71f669fc6e5ec1d0b38e6016366
julie-desvaux <desvaux.julie@gmail.com>
2021-10-19 14:17:35 +0200

    Change title and color

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,29 +1,28 @@
-import React from 'react'
-import CategorySlider from '../Components/Carousels/CategoryCarousel'
-import ArticleCarousel from '../Components/Carousels/ArticleCaousel';
-import "./css/Home.css"
-import Image from "../assets/koya-illustration-impact .svg"
-
+import React from "react";
+import CategorySlider from "../Components/Carousels/CategoryCarousel";
+import ArticleCarousel from "../Components/Carousels/ArticleCaousel";
+// import "./css/Home.css";
+import Image from "../assets/koya-illustration-impact .svg";
 
 const Home = () => {
-    return(
-        <div className="container">
-            <div className="content flex">
-                <div className="w-1/2">
-                    <img src={Image} alt=""/>
-                </div>
-                
-                <div className="w-1/2">
-                    <ArticleCarousel />
-                </div>
-            </div>
+	return (
+		<div className="container pt-14 ">
+			<h1 className="text-center text-4xl font-extrabold text-gray-900">Yakopédia - Le blog de Koya</h1>
+			<div className="pt-14  flex justify-center items-center">
+				<div className="w-1/2 flex justify-center items-center">
+					<img className="w-96" src={Image} alt="" />
+				</div>
 
-            <div className="w-full">
-                <CategorySlider />
-            </div>
-             
-        </div>
-    )
-}
+				<div className="w-1/2">
+					<ArticleCarousel />
+				</div>
+			</div>
 
-export default Home
+			<div className="w-full">
+				<CategorySlider />
+			</div>
+		</div>
+	);
+};
+
+export default Home;
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,13 +1,16 @@
+const colors = require("tailwindcss/colors");
+
 module.exports = {
-  purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
-  darkMode: false, // or 'media' or 'class'
-  theme: {
-    extend: {},
-  },
-  variants: {
-    extend: {},
-  },
-  plugins: [
-    require('@tailwindcss/forms'),
-  ],
-}
+	purge: ["./src/**/*.{js,jsx,ts,tsx}", "./public/index.html"],
+	darkMode: false, // or 'media' or 'class'
+	theme: {
+		colors: {
+			green: colors.teal,
+		},
+		extend: {},
+	},
+	variants: {
+		extend: {},
+	},
+	plugins: [require("@tailwindcss/forms")],
+};
